6a4c33710db4b5beaab14894538b358d76acf5b1
Justyna Nowakowska <nowakowska.just@gmail.com>
2020-10-23 17:51:32 +0200

    Add main folder

---
+++ Documents/Python/Projekt/config.py
@@ -0,0 +1,13 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))    
+
+class Config:
+        SECRET_KEY = os.environ.get("SECRET_KEY") or "szczypior"    
+        SQLALCHEMY_DATABASE_URI = (                           
+                os.environ.get('DATABASE_URL') or
+                'sqlite:///' + os.path.join(BASE_DIR, 'blog.db')
+        )
+        ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
+        ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "szczypior")
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
